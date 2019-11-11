44eb47feb330556c4d9f7c28f050f3241ed931fb
OtakeHikaru <yellowtake59@icloud.com>
2019-11-11 15:21:33 +0900

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '13.112.122.223', user: 'ec2-user', roles: %w{app db web}
+server '3.114.107.124', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
